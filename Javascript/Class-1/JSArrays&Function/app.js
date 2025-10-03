129b39e9d7bc14d24bc40481023f4787ba113e0d
Sabeel Khan <sabeelhps@gmail.com>
2025-10-03 23:29:12 +0530

    JS Intro

---
+++ Javascript/Class-1/JSArrays&Function/app.js
@@ -0,0 +1,51 @@
+// Functions?
+
+// Fuction parameters are x and y
+// function add(x, y) { //function defination
+//     const res = x + y;
+//     return res;
+// }
+
+// // 3 and 4 are the arguments
+// const result = add(3, 4); // function call
+
+// const result1 = add(5, 6); // function call
+
+
+// console.log(result);
+// console.log(result1);
+
+// function simpleInterest(p, r, t) {
+//     return (p * r * t) / 100;
+// }
+
+// console.log(simpleInterest(1000, 10, 2));
+
+
+// -------------------- Arrays ? ----------
+
+// const nums = [1, 2, 3, 4, 5];
+
+// console.log(nums);
+
+
+// console.log(nums.length);
+
+// console.log(nums[3]);
+
+// // ----------------------Objects
+
+// const person = {
+//     name: 'Max',
+//     age: 25,
+//     isAdult: true,
+//     hobbies: ['Swimming', 'Tennis', 'Chess']
+// }
+
+let str = "Hello";
+
+console.log(typeof str);
+
+str = 12345;
+
+console.log(typeof str);
---
+++ Javascript/Class-1/JSVariables/app.js
@@ -0,0 +1,27 @@
+// var a = 100;
+
+// var a = 99;
+
+// console.log(a);
+
+// const declaration are readonly.
+// const b = 200;
+// console.log(b);
+
+// const b = 999;
+
+// console.log(b);
+
+// b = 300;
+
+// console.log(b);
+
+// let keyword
+
+// let x = 100;
+
+// console.log(x);
+
+// x = 500;
+
+// console.log(x);
